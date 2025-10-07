5e95b9b1779fb0c301bae52516131338465aa94c
AL SAIDUL ARMAN MIR <mirarman8583@gmail.com>
2025-10-07 09:42:05 +0600

    override added

--- TestOOP.java
+++ TestOOP.java
@@ -2,47 +2,73 @@ class Vehicle {
     String brand;
     int year;
     double price;
-    
-    Vehicle (String brand, int year, double price){
+
+    Vehicle(String brand, int year, double price) {
         this.brand = brand;
         this.year = year;
         this.price = price;
 
     }
 
-    void displayInfo(){
-        System.out.println("Brand Name: "+brand);
-        System.out.println("Year Release: "+year);
-        System.out.println("Brand Price: "+price);
+    void displayInfo() {
+        System.out.println("Brand Name: " + brand);
+        System.out.println("Year Release: " + year);
+        System.out.println("Brand Price: " + price);
 
     }
 }
 
-
 class Car extends Vehicle {
-    double speed ;
-    Car (String brand, int year , double price, double speed){
+    double speed;
+
+    Car(String brand, int year, double price, double speed) {
         super(brand, year, price);
-        this.speed= speed;
+        this.speed = speed;
     }
 
-    public void displayInfo(){
+    public void displayInfo() {
         super.displayInfo();
-        System.out.println("Top speed of this car: "+speed + "K/H");
+        System.out.println("Top speed of this car: " + speed + "K/H");
     }
+}
+
+class Bus extends Vehicle {
+    double speed;
+
+    Bus(String brand, int year, double price, double speed) {
+        super(brand, year, price);
+        this.speed = speed;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Top speed of this Bus: " + speed + "K/H");
+    }
+
 }
 
 public class TestOOP {
-    public static void main (String[] args){
-        Car redCar =  new Car ("Toyota Supra ", 2003, 40000 , 240);
+    public static void main(String[] args) {
 
-        Car blueCar =  new Car( "Mazda RX7", 2009, 50000 , 260);
+        // Vehicle v =new Vehicle(null, 0, 0)
 
-        Car greenCar =  new Car( "Hennessy Venom F5", 2009, 70000 , 340 );
+        Car redCar = new Car("Toyota Supra ", 2003, 40000, 240);
+
+        Car blueCar = new Car("Mazda RX7", 2009, 50000, 260);
+
+        Car greenCar = new Car("Hennessy Venom F5", 2009, 70000, 340);
 
         redCar.displayInfo();
+        System.out.println("----------------------");
         blueCar.displayInfo();
+        System.out.println("----------------------");
+
         greenCar.displayInfo();
+        System.out.println("----------------------");
+
+        Bus newBus = new Bus("Mercedes Benz", 2009, 700000, 340);
+        newBus.displayInfo();
 
     }
 }
