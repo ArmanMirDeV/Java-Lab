a30140f2f5ad7a8000cf8440d29570361e4235c0
AL SAIDUL ARMAN MIR <mirarman8583@gmail.com>
2025-10-07 09:24:16 +0600

    Add files via upload

--- TestOOP.java
+++ TestOOP.java
@@ -21,18 +21,24 @@ class Vehicle {
 
 class Car extends Vehicle {
     double speed ;
-    Car (String brand, int year , double price){
+    Car (String brand, int year , double price, double speed){
         super(brand, year, price);
+        this.speed= speed;
+    }
+
+    public void displayInfo(){
+        super.displayInfo();
+        System.out.println("Top speed of this car: "+speed + "K/H");
     }
 }
 
 public class TestOOP {
     public static void main (String[] args){
-        Car redCar =  new Car ("Toyota Supra ", 2003, 40000);
+        Car redCar =  new Car ("Toyota Supra ", 2003, 40000 , 240);
 
-        Car blueCar =  new Car( "Mazda RX7", 2009, 50000 );
+        Car blueCar =  new Car( "Mazda RX7", 2009, 50000 , 260);
 
-        Car greenCar =  new Car( "Hennessy Venom F5", 2009, 70000 );
+        Car greenCar =  new Car( "Hennessy Venom F5", 2009, 70000 , 340 );
 
         redCar.displayInfo();
         blueCar.displayInfo();
